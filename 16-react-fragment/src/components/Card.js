24cf5b8090bb7a841e42476a4ffde37aadec7cca
Aleksey.tcherkasoff <136820540+Alekseytcherkasoff@users.noreply.github.com>
2026-05-18 16:26:33 +0700

    Add files via upload

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <React.Fragment>
+      <h1>Aleksey</h1>
+      <h3>I am studying the React course</h3>
+      <button>Like!</button>
+    </React.Fragment>
+  )
+}
+
+export default Card
